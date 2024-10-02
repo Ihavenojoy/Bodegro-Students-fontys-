2c5625d9bda8c51192f6f0a07a0c89c0ebdfb9fd
MartijnKamp <155973854+MartijnKamp@users.noreply.github.com>
2024-10-02 13:43:57 +0200

    NewSubscribsionFile

---
+++ Bodegro/NewSubscription.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Bodegro
+{
+    partial class NewSubscription
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Confirm = new Button();
+            ProtocoBox = new ListBox();
+            PatientBox = new ListBox();
+            StartDate = new DateTimePicker();
+            EndDate = new DateTimePicker();
+            SuspendLayout();
+            // 
+            // Confirm
+            // 
+            Confirm.Location = new Point(610, 393);
+            Confirm.Name = "Confirm";
+            Confirm.Size = new Size(75, 23);
+            Confirm.TabIndex = 0;
+            Confirm.Text = "Bevestig";
+            Confirm.UseVisualStyleBackColor = true;
+            Confirm.Click += Confirm_Click;
+            // 
+            // ProtocoBox
+            // 
+            ProtocoBox.FormattingEnabled = true;
+            ProtocoBox.ItemHeight = 15;
+            ProtocoBox.Location = new Point(43, 37);
+            ProtocoBox.Name = "ProtocoBox";
+            ProtocoBox.Size = new Size(239, 379);
+            ProtocoBox.TabIndex = 1;
+            // 
+            // PatientBox
+            // 
+            PatientBox.FormattingEnabled = true;
+            PatientBox.ItemHeight = 15;
+            PatientBox.Location = new Point(326, 37);
+            PatientBox.Name = "PatientBox";
+            PatientBox.Size = new Size(239, 379);
+            PatientBox.TabIndex = 2;
+            // 
+            // StartDate
+            // 
+            StartDate.Location = new Point(588, 37);
+            StartDate.MinDate = new DateTime(2024, 10, 2, 0, 0, 0, 0);
+            StartDate.Name = "StartDate";
+            StartDate.Size = new Size(200, 23);
+            StartDate.TabIndex = 3;
+            StartDate.Value = new DateTime(2024, 10, 25, 23, 59, 59, 0);
+            // 
+            // EndDate
+            // 
+            EndDate.Location = new Point(588, 66);
+            EndDate.Name = "EndDate";
+            EndDate.Size = new Size(200, 23);
+            EndDate.TabIndex = 4;
+            // 
+            // NewSubscription
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(EndDate);
+            Controls.Add(StartDate);
+            Controls.Add(PatientBox);
+            Controls.Add(ProtocoBox);
+            Controls.Add(Confirm);
+            Name = "NewSubscription";
+            Text = "NewSubscription";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button Confirm;
+        private ListBox ProtocoBox;
+        private ListBox PatientBox;
+        private DateTimePicker StartDate;
+        private DateTimePicker EndDate;
+    }
+}
---
+++ BodegroBusinessLayer/NewSubscriptionDomain.cs
@@ -0,0 +1,116 @@
+﻿using BodegroLibery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BodegroBusinessLayer
+{
+    public class NewSubscriptionDomain
+    {
+        private Doctor doctor;
+        private List<Patient> patients = new List<Patient> { };
+        private List<Protocol> protocols = new List<Protocol> { };
+        public NewSubscriptionDomain(Doctor doctor)
+        {
+            this.doctor = doctor;
+            GetMockData();
+        }
+        public List<string> GivePatients()
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < patients.Count; i++)
+            {
+                list.Add(patients[i].Name);
+            }
+            return list;
+        }
+        public List<string> GiveProtocols()
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < protocols.Count; i++)
+            {
+                list.Add(protocols[i].Name);
+            }
+            return list;
+        }
+        public string AddSubscription(string protocol, string patient, DateTime SDate, DateTime EDate)
+        {
+            if (Datumcheck(SDate, EDate)) 
+            {
+                int proto = 0;
+                for (int i = 0; i < protocols.Count; i++)
+                {
+                    if (protocols[i].Name == protocol) { proto = i; }
+                }
+                Protocol prot = protocols[proto];
+                int patien = 0;
+                for (int i = 0; i < patients.Count; i++)
+                {
+                    if (patients[i].Name == patient) { patien = i; }
+                }
+                Subscription subscription = new Subscription(SDate, EDate, prot);
+                patients[patien].subscriptions.Add(subscription);
+                return "Succesvol toegevoegt";
+            }
+            return "Onverwacht probleem gededecteert";
+        }
+        private void GetMockData()
+        {
+            List<string> medicalHistory = new List<string>();
+            Patient Mock1 = new Patient("Piet", "pietpuk@gmail.com", 45963049, medicalHistory, Regio.Hart_voor_Brabant);
+            Patient Mock2 = new Patient("Henk", "henkklaasen@gmail.com", 54746438, medicalHistory, Regio.Amsterdam);
+            Patient Mock3 = new Patient("Jan", "janjansen@gmail.com", 98379626, medicalHistory, Regio.Brabant_Zuidoost);
+            Patient Mock4 = new Patient("Tom", "tomvandelest@gmail.com", 74725952, medicalHistory, Regio.West_Brabant);
+            patients.Add(Mock1);
+            patients.Add(Mock2);
+            patients.Add(Mock3);
+            patients.Add(Mock4);
+            List<Step> steps = new List<Step>();
+            Protocol Mock5 = new Protocol("Diabetus", steps);
+            Protocol Mock6 = new Protocol("Hepatitus", steps);
+            Protocol Mock7 = new Protocol("Ebola", steps);
+            Protocol Mock8 = new Protocol("HIV", steps);
+            protocols.Add(Mock5);
+            protocols.Add(Mock6);
+            protocols.Add(Mock7);
+            protocols.Add(Mock8);
+        }
+        public bool Datumcheck(DateTime StartDate, DateTime EndDate)
+        {
+            bool check = false;
+            int SDay = Convert.ToInt32(GetDate(StartDate.ToString(), 0, 1));
+            int EDay = Convert.ToInt32(GetDate(EndDate.ToString(), 0, 1));
+            string[] Ssubs = StartDate.ToString().Split('/');
+            string[] Esubs = EndDate.ToString().Split('/');
+            int SMonth = Convert.ToInt32(Ssubs[1]);
+            int EMonth = Convert.ToInt32(Esubs[1]);
+            int SYear = Convert.ToInt32(GetDate(Ssubs[2], 0, 3));
+            int EYear = Convert.ToInt32(GetDate(Esubs[2], 0, 3));
+            if (SDay <= EDay && SMonth <= EMonth && SYear <= EYear)
+            {
+                check = true;
+            }
+            else if (SMonth < EMonth && SYear <= EYear)
+            {
+                check = true;
+            }
+            else if (SYear < EYear)
+            {
+                check = true;
+            }
+            return check;
+        }
+        private string GetDate(string input, int min, int max)
+        {
+            string Date = "";
+            for (int i = min; i <= max; i++)
+            {
+                char temp = input[i];
+                Date += temp;
+            }
+            return Date;
+        }
+    }
+}
