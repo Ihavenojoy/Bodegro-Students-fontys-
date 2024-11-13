e20e71f62c331e262d8b8e1005d94d14b9a76851
Abdullah Tarakji <482864@student.fontys.nl>
2024-11-13 13:51:45 +0100

    Appsettings connectionstring added

---
+++ BodegroASP/BLL/Containers/IDoctorContainer.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Modules;
+
+namespace Domain.Containers
+{
+    public interface IDoctorContainer
+    {
+        int CreateDoctor(Doctor doctor, string password);
+        bool DeleteDoctor(int doctorId);
+        bool DoctorExists(string email);
+        List<Doctor> GetAllDoctors();
+    }
+}
