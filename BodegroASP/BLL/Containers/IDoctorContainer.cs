d51f31ad3ec10f362f45babf20a210d0b76324c9
Ihavenojoy <tom.van.de.lest@gmail.com>
2024-11-13 14:24:47 +0100

    small update

--- BodegroASP/BLL/Containers/IDoctorContainer.cs
+++ BodegroASP/BLL/Containers/IDoctorContainer.cs
@@ -4,7 +4,7 @@ namespace Domain.Containers
 {
     public interface IDoctorContainer
     {
-        int CreateDoctor(Doctor doctor, string password);
+        bool CreateDoctor(Doctor doctor, string password);
         bool DeleteDoctor(int doctorId);
         bool DoctorExists(string email);
         List<Doctor> GetAllDoctors();
