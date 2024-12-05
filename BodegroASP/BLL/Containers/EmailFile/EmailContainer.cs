8c4618f716e36af2852e91856c9c69d1e58591de
MartijnKamp <155973854+MartijnKamp@users.noreply.github.com>
2024-12-05 15:31:10 +0100

    Mailsetup

--- BodegroASP/BLL/Containers/EmailFile/EmailContainer.cs
+++ BodegroASP/BLL/Containers/EmailFile/EmailContainer.cs
@@ -45,47 +45,45 @@ namespace Domain.Containers.EmailFile
 
 
 
-        public MailMessage MailMessage(string emailReciever, EmailBody body)
+        public MailMessage MailMessage(string emailReciever, EmailBody body, string sendString)
         {
             MailMessage mail = new MailMessage();
 
             mail.From = new MailAddress("no.reply.grodebo@gmail.com");
             mail.To.Add(emailReciever);
             mail.Subject = SubjectSetup(body);
-            mail.Body = TextSetup(body);
+            mail.Body = TextSetup(body, sendString);
             mail.IsBodyHtml = true;
 
 
             return mail;
         }
 
-        public string TextSetup(EmailBody body)
+        public string TextSetup(EmailBody body, string sendString)
         {
             switch (body)
             {
                 case EmailBody.TWOFACTOR:
-                    return @"
+                    return @$"
                 <h2>Your Two-Factor Authentication (2FA) Code</h2>
                 <p>Dear <b>User</b>,</p>
                 <p>To complete your login, please use the following authentication code:</p>
-                <h1 style='color:blue;'>{{2FA_CODE}}</h1>
+                <h1 style='color:blue;'>{sendString}</h1>
                 <p>This code will expire in 10 minutes.</p>
                 <p>If you did not request this code, please ignore this email or contact our support team.</p>
                 <p>Best regards,<br/>Your Security Team</p>
             ";
 
                 case EmailBody.APPOINTMENT:
-                    return @"
+                    return @$"
                 <h2>Appointment Confirmation</h2>
-                <p>Dear <b>{{USER_NAME}}</b>,</p>
-                <p>Your appointment has been scheduled as follows:</p>
-                <p><b>Date:</b> {{APPOINTMENT_DATE}}</p>
-                <p><b>Time:</b> {{APPOINTMENT_TIME}}</p>
-                <p><b>Location:</b> {{APPOINTMENT_LOCATION}}</p>
-                <p>If this is a virtual appointment, use the following link to join:</p>
-                <p><a href='{{APPOINTMENT_LINK}}'>Join Appointment</a></p>
+                <p>Dear <b>Patient</b>,</p>
+                <p>With this e-mail we remind you to make a new apointment at your nearest medical facility</p>
+                <p>As of today we will be expecting a new request for an apointment from you</p>
+                <p>You have until the date below to make an apointment</p>
+                <p><b>Date:</b> {sendString}</p>
                 <p>Please contact us if you need to reschedule or have any questions.</p>
-                <p>Best regards,<br/>{{COMPANY_NAME}}</p>
+                <p>Best regards,<br/>Bodegro</p>
             ";
 
                 default:
