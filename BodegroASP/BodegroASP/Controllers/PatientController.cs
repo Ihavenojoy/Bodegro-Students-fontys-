a942cbd0e5334a45a215e9fec4b97a27d670b5fb
MartijnKamp <155973854+MartijnKamp@users.noreply.github.com>
2024-12-18 13:12:16 +0100

    Searchbar

--- BodegroASP/BodegroASP/Controllers/PatientController.cs
+++ BodegroASP/BodegroASP/Controllers/PatientController.cs
@@ -27,7 +27,8 @@ namespace BodegroASP.Controllers
         private ProtocolConvertert ProtocolConverter = new ProtocolConvertert();
         private User user;
         private readonly PatientConvertert patientConverter = new PatientConvertert();
-        private readonly SubscriptionConvertert subscriptionConverter = new SubscriptionConvertert();
+        private readonly SubscriptionConvertert subscriptionConverter = new SubscriptionConvertert(); 
+        SearchService SearchService { get; set; }
         public PatientController()
         {
             _patientserver = new PatientContainer(new PatientDAL(iConfiguration));
@@ -36,12 +37,18 @@ namespace BodegroASP.Controllers
             GetProtocolForPatient = new(new ProtocolDAL(iConfiguration), new StepDAL(iConfiguration));
             user = new User(1, "Tim", "timHaiwan", Role.Doctor, true);
             _stepserver = new(new StepDAL(iConfiguration));
+            SearchService = new(new PatientDAL(iConfiguration), new SubscriptionDAL(iConfiguration));
         }
         public IActionResult Index()
         {
             List<Patient> templist = _patientserver.GetPatientsOfUser(user);
             List<PatientViewModel> list = patientConverter.ListObjectToVieuw(templist);
-            return View(list);
+            PatientFormViewModel model = new PatientFormViewModel()
+            {
+                Patients = list,
+                Search = ""
+            };
+            return View(model);
         }
 
         public IActionResult AddProtocolPatient(PatientViewModel patient)
@@ -86,6 +93,16 @@ namespace BodegroASP.Controllers
                 StepsTaken = 0
             };
             return View(model);  // You can pass the model to a confirmation view
+        }
+        public IActionResult Search(PatientFormViewModel model)
+        {
+            if (model.Search != null && model.Search != "")
+            {
+                List<PatientViewModel> mailInfos = patientConverter.ListObjectToVieuw(SearchService.SearchPatient(model.Search, user));
+                model.Patients = mailInfos;
+                return View("Index", model);
+            }
+            return View();
         }
         [HttpPost]
         public IActionResult SaveSubscription([FromBody] ConfirmProtocolLinkingViewModel subscription)
